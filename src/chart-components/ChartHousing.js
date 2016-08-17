1c13e06e7522ad258a0bc282d8db790c00fcd6b3
Mau <QMau@us.imshealth.com>
2016-08-17 09:22:36 -0700

    shart housing

---
+++ src/chart-components/ChartHousing.js
@@ -0,0 +1,20 @@
+import React from 'react';
+//import all charts
+//import legend component
+
+class ChartHousing extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xs-9">
+            {/*chart goes here*/}
+          </div>
+          <div className="col-xs-3">
+            {/*legend component goes here*/}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
